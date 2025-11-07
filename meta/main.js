4ada318c10b5dbc86f922fedcd08d2a1b6be78fd
cmlpk <capaik@ucsd.edu>
2025-11-07 15:39:35 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -40,8 +40,35 @@ function processCommits(data) {
   });
 }
 
-let data = await loadData();
-let commits = processCommits(data);
+function renderScatterPlot(commits) {
+  const width = 1000;
+  const height = 600;
+  
+  const svg = d3.select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+  
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(commits, d => d.datetime))
+    .range([0, width])
+    .nice();
+
+  const yScale = d3.scaleLinear()
+    .domain([0, 24])
+    .range([height, 0]);
+  
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots.selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', d => xScale(d.datetime))
+    .attr('cy', d => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}
+
 function renderCommitInfo(data, commits) {
   const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
@@ -60,4 +87,8 @@ function renderCommitInfo(data, commits) {
   dl.append('dd').text(Math.round(averageFileLength) + ' lines');
 }
 
+let data = await loadData();
+let commits = processCommits(data);
+
 renderCommitInfo(data, commits);
+renderScatterPlot(commits);
