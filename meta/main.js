b11dcca3a080e66b4484455ab922954b103e06a5
cmlpk <capaik@ucsd.edu>
2025-11-07 15:06:16 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -42,4 +42,14 @@ function processCommits(data) {
 
 let data = await loadData();
 let commits = processCommits(data);
-console.log(commits);
+function renderCommitInfo(data, commits) {
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+}
+
+renderCommitInfo(data, commits);
