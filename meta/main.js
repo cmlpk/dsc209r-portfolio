81b15bf63c1fa4fef9d03a236c5278136f20f0bc
cmlpk <capaik@ucsd.edu>
2025-11-07 16:00:16 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -57,6 +57,19 @@ function renderScatterPlot(commits) {
   const yScale = d3.scaleLinear()
     .domain([0, 24])
     .range([height, 0]);
+
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+    const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+    };
+    xScale.range([usableArea.left, usableArea.right]);
+    yScale.range([usableArea.bottom, usableArea.top]);
   
   const dots = svg.append('g').attr('class', 'dots');
 
@@ -67,6 +80,17 @@ function renderScatterPlot(commits) {
     .attr('cy', d => yScale(d.hourFrac))
     .attr('r', 5)
     .attr('fill', 'steelblue');
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+    svg.append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
 }
 
 function renderCommitInfo(data, commits) {
